73f555cc024685b033090c47476945820332eb25
Tom Black <tom.black06007@gmail.com>
2020-08-14 22:10:01 -0700

    Updates

--- precache-manifest.ce9a10d5a55d05c2521498e683bbce79.js
+++ precache-manifest.ce9a10d5a55d05c2521498e683bbce79.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0c3fae40009d33bf39c45d4480cd8414",
+    "revision": "4e36f5add7d0b653a33193b10fa5cbfe",
     "url": "/Employee-Directory/index.html"
   },
   {
-    "revision": "fd8811df3accc6370ed9",
-    "url": "/Employee-Directory/static/css/main.ed6088f6.chunk.css"
+    "revision": "2291bf679cfa2bc189de",
+    "url": "/Employee-Directory/static/css/main.cb82a0ab.chunk.css"
   },
   {
     "revision": "f77eff5e7b83bd0eb094",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Employee-Directory/static/js/2.68047e47.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fd8811df3accc6370ed9",
-    "url": "/Employee-Directory/static/js/main.b4defc53.chunk.js"
+    "revision": "2291bf679cfa2bc189de",
+    "url": "/Employee-Directory/static/js/main.9e9fa5dc.chunk.js"
   },
   {
     "revision": "2d0c39e313a81ec40abe",
